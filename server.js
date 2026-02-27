6908e79abc222b21e11130c48caef611fd044459
seyyed mobin moosavi <119622416+mobinmsv@users.noreply.github.com>
2026-02-28 00:50:42 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(process.env.PORT || 1000);
+}).listen(process.env.PORT || 10000);
